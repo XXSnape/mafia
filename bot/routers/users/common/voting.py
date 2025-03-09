dac0552fdf188c14be0dcc81b535423af3ce6650
Snape <serg.serg2006@mail.ru>
2025-03-09 20:05:37 +0300

    instigator fixed

--- bot/routers/users/common/voting.py
+++ bot/routers/users/common/voting.py
@@ -8,7 +8,9 @@ from keyboards.inline.callback_factory.recognize_user import (
     UserActionIndexCbData,
     UserVoteIndexCbData,
 )
-from keyboards.inline.keypads.to_bot import participate_in_social_life
+from keyboards.inline.keypads.to_bot import (
+    participate_in_social_life,
+)
 from services.roles import Instigator
 from utils.utils import get_state_and_assign
 
@@ -29,16 +31,16 @@ async def vote_for(
         bot_id=callback.bot.id,
     )
     game_data: GameCache = await game_state.get_data()
+    voted_user_id = game_data["players_ids"][
+        callback_data.user_index
+    ]
     if callback.from_user.id == Instigator().get_processed_user_id(
         game_data
     ):
         ids = game_data["players_ids"][:]
         ids.remove(callback.from_user.id)
+        ids.remove(voted_user_id)
         voted_user_id = choice(ids)
-    else:
-        voted_user_id = game_data["players_ids"][
-            callback_data.user_index
-        ]
     game_data["vote_for"].append(voted_user_id)
     voting_url = game_data["players"][str(callback.from_user.id)][
         "url"
