3864feac921fa10f9d79f32036745a3fa8a19e4f
Snape <serg.serg2006@mail.ru>
2025-03-19 17:37:13 +0300

    the prohibited role's table has been created

--- bot/alembic/env.py
+++ bot/alembic/env.py
@@ -14,6 +14,7 @@ from database.models import (
     UserModel,
     GameModel,
     ResultModel,
+    ProhibitedRoleModel,
 )
 from general import settings
 
--- bot/database/models/__init__.py
+++ bot/database/models/__init__.py
@@ -3,3 +3,4 @@ from .roles import RoleModel
 from .users import UserModel
 from .games import GameModel
 from .results import ResultModel
+from .prohibited_roles import ProhibitedRoleModel
