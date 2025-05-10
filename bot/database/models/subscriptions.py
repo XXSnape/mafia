b58076d97e59913c9029db60c3b1d35d9d95dc3f
Snape <snape@example.com>
2025-05-10 23:00:17 +0300

    Added revisions and restrictions on uniqueness

--- bot/database/models/subscriptions.py
+++ bot/database/models/subscriptions.py
@@ -1,9 +1,16 @@
 from database.common.base import BaseModel, IdMixin
-from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy import BigInteger, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class SubscriptionModel(IdMixin, BaseModel):
+    __table_args__ = (
+        UniqueConstraint(
+            "group_id",
+            "user_tg_id",
+            name="idx_uniq_group_user",
+        ),
+    )
     group_id: Mapped[int] = mapped_column(
         ForeignKey("groups.id", ondelete="CASCADE")
     )
