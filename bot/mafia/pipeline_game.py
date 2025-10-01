bcfee8c65057d6fe863c11f3b29cef7b982d3abc
Snape <email>
2025-10-01 23:06:25 +0300

    Add a second to delete messages

--- bot/mafia/pipeline_game.py
+++ bot/mafia/pipeline_game.py
@@ -216,7 +216,7 @@ class Game:
         game_data = await delete_messages_from_to_delete(
             bot=self.bot, state=self.state
         )
-        await asyncio.sleep(5)
+        await asyncio.sleep(6)
         await self.controller.send_delay_messages(
             game_data=game_data, at_night=True
         )
@@ -233,7 +233,7 @@ class Game:
         game_data = await delete_messages_from_to_delete(
             bot=self.bot, state=self.state
         )
-        await asyncio.sleep(5)
+        await asyncio.sleep(6)
         await self.controller.send_delay_messages(
             game_data=game_data, at_night=False
         )
